afd484562a1638c5e70faa24207ea4509a23b075
Jason Roelofs <jameskilton@gmail.com>
2008-05-09 12:17:31 -0400

    Now takes cxxflags for extra parsing customizability

--- gccxml.rb
+++ gccxml.rb
@@ -7,6 +7,7 @@ class GCCXML
   def initialize()
     @exe = find_exe.strip.chomp
     @includes = []
+    @flags = []
   end
 
   # Add an include path for parsing
@@ -14,12 +15,18 @@ class GCCXML
     @includes << path
   end
 
+  # Add extra CXXFLAGS to the command line
+  def add_cxxflags(flags)
+    @flags << flags
+  end
+
   # Run gccxml on the header file(s), sending the output to the passed in
   # file.
   def parse(header_file, to_file)
     includes = @includes.flatten.uniq.map {|i| "-I#{i.chomp}"}.join(" ").chomp
-    cmd = "#{@exe} #{includes} #{header_file} -fxml=#{to_file}"
-    system(cmd)
+    flags = @flags.flatten.join(" ").chomp
+    cmd = "#{@exe} #{includes} #{flags} #{header_file} -fxml=#{to_file}"
+    raise "Error executing gccxml command line: #{cmd}" unless system(cmd)
   end
 
   private
